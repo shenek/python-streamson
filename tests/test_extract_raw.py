e66ff6574727057edc7bd56e6e83627dabf09cce
Stepan Henek <stepan@henek.name>
2020-08-04 00:19:32 +0200

    fix: extract_iter_raw test retval

--- tests/test_extract_raw.py
+++ tests/test_extract_raw.py
@@ -8,9 +8,9 @@ DATA = [b'{"users": ["john","carl","bob"]}']
 def test_simple():
     matcher = streamson.SimpleMatcher('{"users"}[]')
     extracted = streamson.extract_iter_raw((e for e in DATA), matcher)
-    assert next(extracted) == ('{"users"}[0]', b'"john"')
-    assert next(extracted) == ('{"users"}[1]', b'"carl"')
-    assert next(extracted) == ('{"users"}[2]', b'"bob"')
+    assert next(extracted) == ('{"users"}[0]', '"john"')
+    assert next(extracted) == ('{"users"}[1]', '"carl"')
+    assert next(extracted) == ('{"users"}[2]', '"bob"')
 
     with pytest.raises(StopIteration):
         next(extracted)
